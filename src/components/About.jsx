792684409a0193e3ddb20ee57071e2f6d20a0846
krishna <krishnakoturu526@gmail.com>
2025-08-02 22:02:37 +0530

    about us updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,15 @@ const About = () => {
   return (
     <div>
       <h3>This is krishna from KNB Techs</h3>
+      <div>
+        <h3>Hello Everyone how are you</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          numquam quam dignissimos enim dolorum accusantium pariatur ullam
+          recusandae. Maiores hic eligendi ullam. Ut autem quae quis quisquam
+          praesentium cumque fugiat.
+        </p>
+      </div>
       <button>Submit</button>
     </div>
   );
